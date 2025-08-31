93687f40a32ef3f124ae30dedc67c65004a80d55
alifaizyab8 <alifaizyab8@gmail.com>
2025-08-31 12:14:03 +0500

    Added TimeConverter Exercise 2.20

---
+++ TimeConverter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int input_seconds, hours, minutes, seconds;
+    printf("Enter time in seconds: ");
+    scanf("%d", &input_seconds);
+    hours = input_seconds / 3600;
+    minutes = (input_seconds % 3600) / 60;
+    seconds = (input_seconds % 3600) % 60;
+    printf("The time is %d:%d:%d (hh:mm:ss)\n", hours, minutes, seconds);
+    return 0;
+}
