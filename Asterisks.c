1b9ab5171ea57ab2a5d2e41bbbe6da66f561c004
alifaizyab8 <alifaizyab8@gmail.com>
2025-08-31 12:42:36 +0500

    Added Asteric Shapes Program Exercise 2.21

---
+++ Asterisks.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main()
+{
+    printf("Rectangle of Asterisks:\n");
+    printf("\n*********\n");
+    for (int i = 0; i < 7; i++)
+    {
+        printf("*       *\n");
+    }
+    printf("*********\n");
+
+    printf("\n\nOval of Asterisks:\n");
+    printf("\n   ***   \n");
+    printf(" *     * \n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("*       *\n");
+    }
+    printf(" *     * \n");
+    printf("   ***   \n");
+    printf("\n\nArrow of Asterisks:\n");
+    printf("\n    *    \n");
+    printf("   ***   \n");
+    printf("  *****  \n");
+    for (int i = 0; i < 6; i++)
+    {
+        printf("    *    \n");
+    }
+    
+    printf("\n\nDiamond of Asterisks:\n");
+    printf("\n    *    \n");
+    printf("   * *   \n");
+    printf("  *   *  \n");
+    printf(" *     * \n");
+    printf("*       *\n");
+    printf(" *     * \n");
+    printf("  *   *  \n");
+    printf("   * *   \n");
+    printf("    *    \n");
+    return 0;
+
+}   
