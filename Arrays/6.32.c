137369c6cafaf0f5ea68cfed52972d23c81c3afb
alifaizyab8 <alifaizyab8@gmail.com>
2025-11-04 18:29:54 +0500

    gpt improvement

--- Arrays/6.32.c
+++ Arrays/6.32.c
@@ -10,14 +10,14 @@ int linearSearch(int arr[], int size, int key)
     {
         return -1;
     }
-    linearSearch(arr,size,key);
+    return linearSearch(arr,size,key);
     
 }
 int main()
 {
     //recursive linear search
     int arr[SIZE] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
-    int key = 10;
-    printf("%d",linearSearch(arr,SIZE,key));
+    int key = 20;
+    printf("%d\n",linearSearch(arr,SIZE,key));
     return 0;
 }
