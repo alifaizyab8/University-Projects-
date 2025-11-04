357da9265c78466a4f2fbe01d551b27c8076504f
alifaizyab8 <alifaizyab8@gmail.com>
2025-11-04 20:13:08 +0500

    NEW BINARY SEARCH

--- Arrays/6.33.c
+++ Arrays/6.33.c
@@ -1,15 +1,24 @@
 #include<stdio.h>
 int binarySearch(int arr[],int low, int high,int key)
 {
-    if (arr[high] == key) return high;
-    if (arr[low] == key) return low;
-    if (high == low) return -1;
-    if (arr[(high + low) / 2] > key)
+    if (high<low)
     {
-        high = (high +low) / 2;
-    }else
-        low = (high + low ) / 2;
-    return binarySearch(arr,low,high,key);
+        return -1;
+    }
+    int mid = (high  + low)/2;
+    if (arr[mid] == key)
+    {
+        return mid;
+    }
+    else if (arr[mid] < key)
+    {
+        return binarySearch(arr,mid+1,high,key);
+    }
+    else
+    {
+        return binarySearch(arr,low,mid-1,key);
+    }
+    
     
     
 }
@@ -18,6 +27,6 @@ int main()
     int arr[10] = {1,2,3,4,5,6,7,8,9,10};
     int high = 9;
     int low = 0;
-    int key = 9;
+    int key = 11;
     printf("%d",binarySearch(arr,low,high,key));
 }
