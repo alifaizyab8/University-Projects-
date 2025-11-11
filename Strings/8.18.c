01d815e91612a79211107f096de90a71b68c60fe
alifaizyab8 <alifaizyab8@gmail.com>
2025-11-12 00:52:37 +0500

    added 2 programs

--- Strings/8.18.c
+++ Strings/8.18.c
@@ -3,7 +3,36 @@
 
 int main()
 {
-    // Exercise 8.18
-    
+    // count the occurence of vowels, consonants, digits and whitespaces in a string using only stdio.h
+    char str[100];
+    int vowels = 0, consonants = 0, digits = 0, whitespaces = 0;
+    char ch;
+    printf("Enter a string: ");
+    int i = 0;
+    while((str[i] = getchar())!= '\n' && i <100)
+        i++;
+
+    str[i] = '\0';
+    for(int j = 0; j < i; j++)
+    {
+        ch = str[j];
+        if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+        {
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+               ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+                vowels++;
+            else
+                consonants++;
+        }
+        else if(ch >= '0' && ch <= '9')
+            digits++;
+        else if(ch == ' ' || ch == '\t')
+            whitespaces++;
+    }
+    printf("Vowels: %d\n", vowels);
+    printf("Consonants: %d\n", consonants);
+    printf("Digits: %d\n", digits);
+    printf("Whitespaces: %d\n", whitespaces);
+        
     return 0;
 }
--- Strings/8.20.c
+++ Strings/8.20.c
@@ -4,6 +4,42 @@
 int main()
 {
     // Exercise 8.20
-    
+    //count the number of words in a string without using string.h
+    char str[100];
+    int words = 0;
+    char ch;
+    printf("Enter a string: ");
+    int i = 0;
+    while((str[i] = getchar())!= '\n' && i <100)
+        i++;
+    str[i] = '\0';
+    // int inWord = 0;
+    // for(int j = 0; j < i; j++)
+    // {
+    //     ch = str[j];
+    //     if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+    //     {
+    //         if(inWord == 0)
+    //         {
+    //             words++;
+    //             inWord = 1;
+    //         }
+    //     }
+    //     else
+    //     {
+    //         inWord = 0;
+    //     }
+    // }
+    // printf("Number of words: %d\n", words);
+    //implementation using countr for whitespaces and enters
+    int spaces = 0;
+    for(int j = 0; j < i; j++)
+    {
+        ch = str[j];
+        if(ch == ' ' || ch == '\t' || ch == '\n')
+            spaces++;
+    }
+    printf("Number of words: %d\n", spaces + 1);
+    // Adding 1 to count the last word if string is not empty
     return 0;
 }
