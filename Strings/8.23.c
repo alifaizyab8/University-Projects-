52f3c7ef81f597047d4a74c145cd80b8ad0e9107
alifaizyab8 <alifaizyab8@gmail.com>
2025-11-13 22:44:49 +0500

    th and tion problems

--- Strings/8.23.c
+++ Strings/8.23.c
@@ -5,6 +5,31 @@ int main()
 {
     // Exercise 8.23
     // Program that takes in series of strings meaning it takes multiple inputs first and only prints those starting with "Th"
-    
+    char str[10][100];
+    printf("Number of inputs: \n");
+    int n;
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    {
+        int j = 0;
+        while ((str[i][j] = getchar()) != '\n' && j < 99)
+            j++;
+        str[i][j] = '\0';
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if ((str[i][0] == 'T') && (str[i][1] == 'h'))
+        {
+
+            for (int k = 0; str[i][k] != '\0'; k++)
+            {
+
+                printf("%c", str[i][k]);
+            }
+            printf("\n");
+        }
+        
+    }
+
     return 0;
 }
--- Strings/8.24.c
+++ Strings/8.24.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
     // Exercise 8.24
-    
+    // only print the string that end in 'tion'
+    char str[10][100];
+    printf("Number of inputs: \n");
+    int n;
+    scanf("%d", &n);
+    getchar(); // clear buffer
+
+    for (int i = 0; i < n; i++)
+    {
+        int j = 0;
+        printf("Enter string %d\n", i + 1);
+        while ((str[i][j] = getchar()) != '\n' && j < 99)
+            j++;
+        str[i][j] = '\0';
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int size = 0;
+        while (str[i][size] != '\0')
+            size++;
+
+        if ((size >= 5) && (str[i][size - 1] == 'n') && (str[i][size - 2] == 'o') && (str[i][size - 3] == 'i') && (str[i][size - 4] == 't'))
+        {
+            printf("%s", str[i]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
